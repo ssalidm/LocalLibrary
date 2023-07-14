5873dda2ce6eac15174f85e000c6ba04fe2d17ab
David Ssali <ssalidm@gmail.com>
2023-07-14 20:14:55 +0200

    Added generic list and detail views

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,7 @@
+from typing import Any, Dict
+from django.db.models.query import QuerySet
 from django.shortcuts import render
+from django.views import generic
 from . models import Book, Author, BookInstance, Genre
 
 # Create your views here.
@@ -18,7 +21,7 @@ def index(request):
 
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
-    
+
     # Books that contain the word 'harry'
     num_harry = Book.objects.filter(title__icontains='harry').count()
 
@@ -27,9 +30,42 @@ def index(request):
         "num_instances": num_instances,
         "num_instances_available": num_instances_available,
         "num_authors": num_authors,
-        "num_genres" : num_genres,
-        "num_harry" : num_harry,
+        "num_genres": num_genres,
+        "num_harry": num_harry,
     }
 
     # Render the HTML template index.html with the data in the context variable
-    return render(request, "index.html", context = context)
+    return render(request, "index.html", context=context)
+
+
+class BookListView(generic.ListView):
+    # model = Book
+    # context_object_name = "book_list" # your own name for the list as a template variable
+    # queryset = Book.objects.filter(title__icontains="harry")[:5] # Get 5 books containing the title harry
+    # template_name = "book/book_list.html" # Specify your own template name/location
+
+    # OR you can override some methods
+    def get_queryset(self):
+        # return Book.objects.filter(title__icontains="harry")[:5] # Get 5 books containing te title harry
+        return Book.objects.all()
+
+    def get_context_data(self, **kwargs):
+        # call the base implementation first to get the context
+        context = super(BookListView, self).get_context_data(**kwargs)
+        # Create any data and add it to the context
+        context["some_data"] = "This is just some data"
+        return context
+
+    paginate_by = 5
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 5
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
