bcf24e40580b66eed5bc2ff4a53b0ac225a02da5
David Ssali <ssalidm@gmail.com>
2023-07-24 14:46:14 +0200

    Add latest changes

--- catalog/urls.py
+++ catalog/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
     path('all_borrowed_books/', views.LoanedBooksListView.as_view(), name='all-borrowed'),
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,10 +1,13 @@
-from typing import Any, Dict
+import datetime
+from django.contrib.auth.decorators import login_required, permission_required
 from django.db.models.query import QuerySet
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.views import generic
 from . models import Book, Author, BookInstance, Genre
 from django.contrib.auth.mixins import LoginRequiredMixin
-# from django.contrib.auth.decorators import login_required
+from catalog.forms import RenewBookForm
 
 # Create your views here.
 
@@ -88,15 +91,49 @@ class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
             BookInstance.objects.filter(borrower=self.request.user).filter(
                 status__exact='o').order_by('due_back')
         )
-        
+
 class LoanedBooksListView(LoginRequiredMixin, generic.ListView):
-    """Generic class-based view listing all books loaned out from the library."""  
+    """Generic class-based view listing all books loaned out from the library."""
     model = BookInstance
-    template_name = 'catalog/bookinstance_list_borrowed_all.html'  
+    template_name = 'catalog/bookinstance_list_borrowed_all.html'
     paginate_by = 10
-    
+
     def get_queryset(self):
         return (
             BookInstance.objects.all().filter(
                 status__exact='o').order_by('due_back')
         )
+
+
+@login_required
+@permission_required('catalog.can_mark_returned', raise_exception=True)
+def renew_book_librarian(request, pk):
+    """View function for renewing a specific BookInstance by librarian."""
+    book_instance = get_object_or_404(BookInstance, pk=pk)
+
+    # If this a POST request, then process the form data
+    if request.method == 'POST':
+        # Create a form instance and populate it with data from the request (binding):
+        form = RenewBookForm(request.POST)
+
+        # Check if the form is valid
+        if form.is_valid():
+            # process the data in form.cleaned_data as required (here we just write it to the model due_back field)
+            book_instance.due_back = form.cleaned_data['renewal_date']
+            book_instance.save()
+
+            # redirect to a new URL
+            return HttpResponseRedirect(reverse('all-borrowed'))
+
+        # If this is GET (or any other method) create the default form
+        else:
+            proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
+            form = RenewBookForm(
+                initial={'renewal_date': proposed_renewal_date})
+
+            context = {
+                'form': form,
+                'book_instance': book_instance,
+            }
+
+            return render(request, 'catalog/book_renew_librarian.html', context)
